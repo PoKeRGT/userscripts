f970fbcf4a7a4283e0e90db5dd17bdb351eabd66
Areyuel <pruebasraul2014@gmail.com>
2026-02-03 00:13:30 +0100

    feat: Update Netflix Auto-Skip script version and improve button click handling

--- scripts/netflix-auto-skip.user.js
+++ scripts/netflix-auto-skip.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Netflix seamless play
 // @namespace    PoKeRGT
-// @version      1.03
+// @version      1.1.0
 // @description  Skips intro, recaps and jumps to next video automatically
 // @author       PoKeRGT
-// @match        https://www.netflix.com/*
+// @match        https://www.netflix.com/watch/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=netflix.com
 // @grant        none
-// @run-at       document-ready
+// @run-at       document-end
 // @homepageURL  https://github.com/PoKeRGT/userscripts
 // @license      MIT
 // ==/UserScript==
@@ -16,38 +16,60 @@
 (function () {
   'use strict';
 
+  const DEBUG = false;
+  const log = (...args) => DEBUG && console.log('[Netflix Auto-Skip]', ...args);
+
   const nextEpisodeButtonXPath = "//button[@data-uia='next-episode-seamless-button-draining']"
   const skipIntroButtonXPath = "//button[@data-uia='player-skip-intro']"
   const skipRecapButtonXPath = "//button[@data-uia='player-skip-recap']"
+
+  log('Script initialized')
+
+  const clickedButtons = new Map()
+  const COOLDOWN_MS = 2000
 
   function getElementByXpath(path, from) {
     return document.evaluate(path, from, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
   }
 
+  function shouldClickButton(buttonId) {
+    const now = Date.now()
+    if ((now - (clickedButtons.get(buttonId) || 0)) < COOLDOWN_MS) {
+      log('Button', buttonId, 'in cooldown, skipping click')
+      return false
+    }
+    clickedButtons.set(buttonId, now)
+    return true
+  }
+
   const observer = new MutationObserver(function (mutations) {
-    mutations.forEach(function (mutation) {
-      mutation.addedNodes.forEach(function (node) {
-        if (node.nodeName === 'DIV') {
+    for (const mutation of mutations) {
+      for (const node of mutation.addedNodes) {
+        if (node.nodeType !== Node.ELEMENT_NODE) continue;
 
-          const skipIntroButton = getElementByXpath(skipIntroButtonXPath, node)
-          const nextEpisodeButton = getElementByXpath(nextEpisodeButtonXPath, node)
-          const skipRecapButton = getElementByXpath(skipRecapButtonXPath, node)
+        const skipIntroButton = getElementByXpath(skipIntroButtonXPath, node)
+        const nextEpisodeButton = getElementByXpath(nextEpisodeButtonXPath, node)
+        const skipRecapButton = getElementByXpath(skipRecapButtonXPath, node)
 
-          if (skipIntroButton) {
-            skipIntroButton.click()
-          }
-
-          if (nextEpisodeButton) {
-            nextEpisodeButton.click()
-          }
-
-          if (skipRecapButton) {
-            skipRecapButton.click()
-          }
+        if (skipIntroButton && shouldClickButton('skipIntro')) {
+          log('Clicking Skip Intro button')
+          skipIntroButton.click()
         }
-      });
-    });
+
+        if (nextEpisodeButton && shouldClickButton('nextEpisode')) {
+          log('Clicking Next Episode button')
+          nextEpisodeButton.click()
+        }
+
+        if (skipRecapButton && shouldClickButton('skipRecap')) {
+          log('Clicking Skip Recap button')
+          skipRecapButton.click()
+        }
+      }
+    }
   });
 
+  log('Starting observer...')
   observer.observe(document, { childList: true, subtree: true });
+  log('Observer started successfully')
 })();
